6526ea96599395124a525f02fd7097b114750c97
abou-zithar <mahmouazbouzithar@gmail.com>
2025-01-13 22:05:25 +0200

    bluePrintSteps

--- pipelines/trainning_pipeline.py
+++ pipelines/trainning_pipeline.py
@@ -4,7 +4,7 @@ from steps.clean_data import clean_df
 from  steps.evaluation import evaluate_model
 from steps.model_train import train_model
 
-@pipeline
+@pipeline(enable_cache=True)
 def training_pipeline(data_path : str):
     df = ingest_df(data_path)
     clean_df(df)
